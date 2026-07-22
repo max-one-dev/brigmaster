144c3f9ce77bb1f9def4c229dae9f23598a51872
Maxim Kirilin <kirilin.1997@mail.ru>
2026-07-22 16:55:41 +0300

    chore(tooling): add eslint + pragmatic phpcs to constructly-core, drop stray git backup

---
+++ plugins/constructly-core/assets/eslint.config.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+      eqeqeq: ['error', 'smart'],
+    },
+  },
+];
